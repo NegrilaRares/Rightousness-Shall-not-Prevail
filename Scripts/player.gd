a826a69ff86de592bd47ffc37b5e65ebcfbb79d6
Negrila Rares <rares_negrila@yahoo.com>
2025-02-18 17:34:33 +0200

    added tutorial wip
    needs story and door to finish lvl 0

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,24 +1,22 @@
 extends CharacterBody2D
 
 var speed : int
-var game_start = false;
+var local_game_start : bool = false
 
 func _ready():
 	speed = 200
-
-
+	Menu.game_start.connect(on_button_play_pressed_received)
+	
+func on_button_play_pressed_received():
+	local_game_start = true
+	
 
 func get_input():
 	
-	
-	if game_start :
-	
+	if local_game_start:
 		#keyboard input
 		var input_dir = Input.get_vector("left", "right", "up", "down")
-		velocity = input_dir.normalized() * speed;
-		
-		
-			 
+		velocity = input_dir.normalized() * speed; 
 		
 		if Input.is_key_pressed(KEY_W):
 			$AnimatedSprite2D.animation = "walk_up"
@@ -33,13 +31,3 @@ func _physics_process(_delta):
 	#player movement
 	get_input()
 	move_and_slide()
-
-
-func _on_button_play_pressed() -> void:
-	
-	game_start = true;
-	
-	$/root/Menu/walls/button_exit.hide();
-	$/root/Menu/walls/button_play.hide();
-	
-	pass # Replace with function body.
