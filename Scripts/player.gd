d2c9391eacc359c0ccec57e2aa8c6b7725562f56
Negrila Rares <129112753+NegrilaRares@users.noreply.github.com>
2025-02-19 17:34:10 +0200

    Revert "melee + Y-Sort"
    This reverts commit 98a3786bcebe118d83b6fe91d677ca63738a700d.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,57 +1,33 @@
 extends CharacterBody2D
 
 var speed : int
-var game_start = false
+var local_game_start : bool = false
 
 func _ready():
 	speed = 200
+	Menu.game_start.connect(on_button_play_pressed_received)
+	
+func on_button_play_pressed_received():
+	local_game_start = true
+	
 
 func get_input():
-
-	if game_start :
 	
-		var enemy = get_node("../Player2/AnimatedSprite2D_2")
-		var player = $AnimatedSprite2D
-	
+	if local_game_start:
 		#keyboard input
 		var input_dir = Input.get_vector("left", "right", "up", "down")
-		velocity = input_dir.normalized() * speed
-		
-		
-		# Change Sprite + Attack HitBox direction (relative to player node (parent) )
+		velocity = input_dir.normalized() * speed 
 		
 		if Input.is_key_pressed(KEY_W):
 			$AnimatedSprite2D.animation = "walk_up"
-			$AnimatedSprite2D/HitBox/CollisionShape2D.position.x = -1
-			$AnimatedSprite2D/HitBox/CollisionShape2D.position.y = -17
-			print($AnimatedSprite2D/HitBox/CollisionShape2D.position)	
-			
 		if Input.is_key_pressed(KEY_A):
 			$AnimatedSprite2D.animation = "walk_left"
-			$AnimatedSprite2D/HitBox/CollisionShape2D.position.x = -32
-			$AnimatedSprite2D/HitBox/CollisionShape2D.position.y = 16
-			print($AnimatedSprite2D/HitBox/CollisionShape2D.position)	
 		if Input.is_key_pressed(KEY_S):
 			$AnimatedSprite2D.animation = "walk_down"
-			$AnimatedSprite2D/HitBox/CollisionShape2D.position.x = -1
-			$AnimatedSprite2D/HitBox/CollisionShape2D.position.y = 49
-			print($AnimatedSprite2D/HitBox/CollisionShape2D.position)	
 		if Input.is_key_pressed(KEY_D):
 			$AnimatedSprite2D.animation = "walk_right"
-			$AnimatedSprite2D/HitBox/CollisionShape2D.position.x = 32
-			$AnimatedSprite2D/HitBox/CollisionShape2D.position.y = 16
-			print($AnimatedSprite2D/HitBox/CollisionShape2D.position)	
-	
 
 func _physics_process(_delta):
 	#player movement
 	get_input()
 	move_and_slide()
-
-func _on_button_play_pressed() -> void:
-	
-	game_start = true
-	
-	$/root/Menu/walls/button_exit.hide()
-	$/root/Menu/walls/button_play.hide()
-	
--- button_exit.gd
+++ button_exit.gd
@@ -3,7 +3,7 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,9 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	# Close game
 	get_tree().quit()
-	
-	
-	
-	
