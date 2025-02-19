14bbd89f4f672914105c83e80234b81b45755a19
Negrila Rares <129112753+NegrilaRares@users.noreply.github.com>
2025-02-19 17:34:10 +0200

    Revert "start game signal moved into Menu"
    This reverts commit 950f4a73367f07d680f089e94ad7e7ae866c5a9d.

--- menu.gd
+++ menu.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
-signal game_start
 
 
-func _on_button_play_pressed() -> void:
-	$walls/button_play.hide()
-	$walls/button_exit.hide()
-	Menu.game_start.emit()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	stage.get_node("Player2").position
+	
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	pass
